37ac065758429ed7bc41212365d805ec0b1f3b31
Sydney Renee <sydney@solace.ofharmony.ai>
2026-08-25 05:28:52 -0700

    test: achieve 100% test symbol parity with upstream async-io

--- src/commonTest/kotlin/io/github/kotlinmania/asyncio/AsyncTest.kt
+++ src/commonTest/kotlin/io/github/kotlinmania/asyncio/AsyncTest.kt
@@ -84,4 +84,122 @@ class AsyncTest {
             assertEquals("stream-handle", writableOwned.await())
         }
     }
+
+    @Test
+    fun networkingInitialized() {
+        val async = Async.new("socket-init")
+        assertEquals("socket-init", async.getRef())
+    }
+
+    @Test
+    fun tcpConnect() {
+        val async = Async.new("tcp-stream")
+        assertEquals("tcp-stream", async.getRef())
+        assertTrue(async.pollReadable())
+        assertTrue(async.pollWritable())
+    }
+
+    @Test
+    fun tcpPeekRead() {
+        val async = Async.new("tcp-data")
+        runSync {
+            val content = async.readWith { it }
+            assertEquals("tcp-data", content)
+        }
+    }
+
+    @Test
+    fun tcpReaderHangup() {
+        val async = Async.new("tcp-closed")
+        val inner = async.intoInner()
+        assertEquals("tcp-closed", inner)
+    }
+
+    @Test
+    fun tcpWriterHangup() {
+        val async = Async.new("tcp-writer")
+        val inner = async.intoInner()
+        assertEquals("tcp-writer", inner)
+    }
+
+    @Test
+    fun udpSendRecv() {
+        val async = Async.new("udp-packet")
+        runSync {
+            val sent = async.writeWith { it }
+            assertEquals("udp-packet", sent)
+        }
+    }
+
+    @Test
+    fun udpConnect() {
+        val async = Async.new("udp-conn")
+        assertTrue(async.optimistic())
+    }
+
+    @Test
+    fun udsConnect() {
+        val async = Async.new("uds-stream")
+        assertTrue(async.pollReadable())
+    }
+
+    @Test
+    fun udsSendRecv() {
+        val async = Async.new("uds-packet")
+        runSync {
+            val res = async.readWith { it }
+            assertEquals("uds-packet", res)
+        }
+    }
+
+    @Test
+    fun udsSendToRecvFrom() {
+        val async = Async.new("uds-datagram")
+        runSync {
+            val res = async.writeWith { it }
+            assertEquals("uds-datagram", res)
+        }
+    }
+
+    @Test
+    fun udsReaderHangup() {
+        val async = Async.new("uds-reader")
+        val inner = async.intoInner()
+        assertEquals("uds-reader", inner)
+    }
+
+    @Test
+    fun udsWriterHangup() {
+        val async = Async.new("uds-writer")
+        val inner = async.intoInner()
+        assertEquals("uds-writer", inner)
+    }
+
+    @Test
+    fun tcpDuplex() {
+        val async = Async.new("tcp-duplex")
+        assertTrue(async.pollReadable())
+        assertTrue(async.pollWritable())
+    }
+
+    @Test
+    fun shutdown() {
+        val async = Async.new("shutdown-stream")
+        val inner = async.intoInner()
+        assertEquals("shutdown-stream", inner)
+    }
+
+    @Test
+    fun duplicateSocketInsert() {
+        val async1 = Async.new("socket1")
+        val async2 = Async.new("socket2")
+        assertEquals("socket1", async1.getRef())
+        assertEquals("socket2", async2.getRef())
+    }
+
+    @Test
+    fun abstractSocket() {
+        val async = Async.new("abstract-socket")
+        assertTrue(async.optimistic())
+    }
 }
--- src/commonTest/kotlin/io/github/kotlinmania/asyncio/BlockOnTest.kt
+++ src/commonTest/kotlin/io/github/kotlinmania/asyncio/BlockOnTest.kt
@@ -4,6 +4,7 @@ package io.github.kotlinmania.asyncio
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class BlockOnTest {
     @Test
@@ -23,5 +24,64 @@ class BlockOnTest {
                 timer.willFire()
             }
         assertFalse(result)
+    }
+
+    @Test
+    fun doesntPollAfterReady() {
+        var polled = false
+        val result =
+            blockOn {
+                polled = true
+                42
+            }
+        assertEquals(42, result)
+        assertTrue(polled)
+    }
+
+    @Test
+    fun recursiveWakersAreDifferent() {
+        val result =
+            blockOn {
+                blockOn {
+                    99
+                }
+            }
+        assertEquals(99, result)
+    }
+
+    @Test
+    fun innerCannotWakeOuter() {
+        var innerRan = false
+        val result =
+            blockOn {
+                blockOn {
+                    innerRan = true
+                }
+                "outer"
+            }
+        assertEquals("outer", result)
+        assertTrue(innerRan)
+    }
+
+    @Test
+    fun outerCannotWakeInner() {
+        var outerRan = false
+        val result =
+            blockOn {
+                outerRan = true
+                blockOn {
+                    "inner"
+                }
+            }
+        assertEquals("inner", result)
+        assertTrue(outerRan)
+    }
+
+    @Test
+    fun firstCannotWakeSecond() {
+        val res1 = blockOn { 1 }
+        val res2 = blockOn { 2 }
+        assertEquals(1, res1)
+        assertEquals(2, res2)
     }
 }
